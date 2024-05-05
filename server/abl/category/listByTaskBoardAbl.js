21dbcc1de6ebb7232714e018064ebb2837869de5
Větříšek Jan <vetrisek.jan@gmail.com>
2024-05-05 20:02:42 +0200

    Frontend

--- server/abl/category/listByTaskBoardAbl.js
+++ server/abl/category/listByTaskBoardAbl.js
@@ -3,14 +3,22 @@ const taskBoardDao = require("../../dao/taskBoard-dao.js");
 
 async function listByTaskBoard(req, res){
     try{
+<<<<<<< HEAD
+        const reqParam = req.query?.taskBoardId ? req.query : req.body;
+=======
         const reqParam = req.query?.id ? req.query : req.body;
+>>>>>>> 5048213d4749f70c9077ae9ab0a5005e6ea62cfd
 
         // TaskBoard existence
         const taskBoardValidation = taskBoardDao.get(reqParam.taskBoardId);
         if (!taskBoardValidation){
             res.status(404).json({
                 code: "taskBoardNotFound",
+<<<<<<< HEAD
+                message: `TaskBoard ${req.taskBoardId} not found`
+=======
                 message: `TaskBoard ${reqParam.taskBoardId} not found`
+>>>>>>> 5048213d4749f70c9077ae9ab0a5005e6ea62cfd
               });
               return;
         }
